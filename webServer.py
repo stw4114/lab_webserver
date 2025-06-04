41e85664951af2175e5033d2f273fc1ebfc22ab7
stw4114 <stw4114@nyu.edu>
2025-06-04 02:39:28 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -10,63 +10,55 @@ def webServer(port=13331):
     # Prepare a server socket
     serverSocket.bind(("", port))
 
-    # Fill in start
-
-    # Fill in end
+    # Start listening with a queue of 1 connection
+    serverSocket.listen(1)
 
     while True:
         # Establish the connection
-
         print('Ready to serve...')
-        connectionSocket, addr =  # Fill in start -are you accepting connections?     #Fill in end
+        connectionSocket, addr = serverSocket.accept()
 
         try:
-            message =  # Fill in start -a client is sending you a message   #Fill in end
+            message = connectionSocket.recv(1024).decode()
             filename = message.split()[1]
 
-            # opens the client requested file.
-            # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-            f = open(filename[1:],  # fill in start #fill in end)
-                     # fill in end
+            # opens the client requested file in binary mode
+            f = open(filename[1:], "rb")
+            file_content = f.read()
+            f.close()
 
-                     # This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
-                     # Fill in start
+            # HTTP response header
+            header = (
+                "HTTP/1.1 200 OK\r\n"
+                "Server: MySimplePythonServer/1.0\r\n"
+                "Content-Type: text/html; charset=UTF-8\r\n"
+                f"Content-Length: {len(file_content)}\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+            ).encode()
 
-                     # Content-Type is an example on how to send a header as bytes. There are more!
-                     outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
+            # Send header + content
+            connectionSocket.send(header + file_content)
 
-            # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-
-            # Fill in end
-
-            for i in f:  # for line in file
-            # Fill in start - append your html file contents #Fill in end
-
-            # Send the content of the requested file to the client (don't forget the headers you created)!
-            # Send everything as one send command, do not send one line/item at a time!
-
-            # Fill in start
-
-            # Fill in end
-
-                connectionSocket.close()  # closing the connection socket
+            connectionSocket.close()  # closing the connection socket
 
         except Exception as e:
-    # Send response message for invalid request due to the file not being found (404)
-    # Remember the format you used in the try: block!
-    # Fill in start
+            # 404 Not Found response
+            error_message = (
+                "<html><body><h1>404 Not Found</h1></body></html>"
+            ).encode()
+            header = (
+                "HTTP/1.1 404 Not Found\r\n"
+                "Server: MySimplePythonServer/1.0\r\n"
+                "Content-Type: text/html; charset=UTF-8\r\n"
+                f"Content-Length: {len(error_message)}\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+            ).encode()
 
-    # Fill in end
+            connectionSocket.send(header + error_message)
 
-    # Close client socket
-    # Fill in start
-
-    # Fill in end
-
-    # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop.
-    # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
-    # serverSocket.close()
-    # sys.exit()  # Terminate the program after sending the corresponding data
+            connectionSocket.close()
 
 
 if __name__ == "__main__":
